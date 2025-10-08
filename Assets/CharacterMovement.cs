a02e1e7af862d9dc75f70c076401ed10be6edcc9
fantastic2021 <jonasmayer.1305@gmail.com>
2025-10-08 16:19:52 +0200

    Second Update Unity
    All changes Jonas (added Shader, Physics, character and IK)

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,21 @@
+using Unity.Hierarchy;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    float dx = 0f;
+    float dy = 0f;
+    float gravity = -50f;
+    void Start()
+    {
+        transform.position = new Vector3(-200, 0, 0); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(dx, dy, 0) * Time.deltaTime);
+        dy += gravity * Time.deltaTime; 
+
+    }
+}
