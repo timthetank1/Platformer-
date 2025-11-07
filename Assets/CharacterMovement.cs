da53d6549d35496f0a17e680f2d8e7f2e4a75340
SimonHarrieder <simon@harrieder.com>
2025-11-07 14:12:25 +0100

    input system
    added input system

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -1,16 +1,40 @@
 using Unity.Hierarchy;
 using UnityEngine;
+<<<<<<< Updated upstream
 
 public class CharacterMovement : MonoBehaviour
 {
     Rigidbody RigidBody;
     void Start()
+=======
+using UnityEngine.InputSystem;
+using System;
+
+public class CharacterMovement : MonoBehaviour
+{ 
+    public Rigidbody2D rb;
+    public InputAction controls;
+    public float speed = 100;
+
+    private void Start()
+>>>>>>> Stashed changes
     {
         RigidBody = GetComponent <RigidBody>();
     }
     void FixedUpdate()
     {
+<<<<<<< Updated upstream
         RigidBody.AddForce(new Vector2(10f, 0f));
+=======
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+        rb.linearVelocity = dir * speed;
+    }
+    private void FixedUpdate()
+    {
+
+>>>>>>> Stashed changes
     }
 }
  
