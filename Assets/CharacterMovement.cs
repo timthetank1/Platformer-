206d23b9e17324e28d2756dd9be3d18e351f2f96
SimonHarrieder <simon@harrieder.com>
2025-11-26 14:10:41 +0100

    started to correct jump

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -3,12 +3,13 @@ using Unity.Hierarchy;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed = 10f;
     [SerializeField] private float jumpForce = 5f;
-
+    private bool groundTouch = false;
 
     [SerializeField] public InputActionReference move;
     [SerializeField] public InputActionReference jump;
@@ -25,20 +26,39 @@ public class CharacterMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    
+
+
     private void FixedUpdate()
     {
         InputDir = move.action.ReadValue<Vector2>();
         x = InputDir[0];
         JumpDir = jump.action.ReadValue<float>();
-        y = JumpDir * jumpForce;
+        if (groundTouch & (jump.action.ReadValue<float>()== 1))
+        {
+            y = jumpForce;
+        }
+        else 
+        {
+            y = 0; 
+        }
 
-
-
-
+        Debug.Log(groundTouch);
         Vector2 dir = new Vector2(x * speed, rb.linearVelocityY + y);
         rb.linearVelocity = dir;
 
     }
 
-    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            groundTouch = true;
+        }
+        else
+        {
+            groundTouch = false;
+        }
+    }
+
 }
