702324ab9b32246efbd735fcb5cf3b4bd0ebf760
fantastic2021 <jonasmayer.1305@gmail.com>
2025-12-04 20:01:54 +0100

    did stuff
    cool stuff

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     [SerializeField] private PlayerStateManager psm;
-    [SerializeField] private int screenSnapX = 128;
-    [SerializeField] private int screenSnapY = 81;
+    private int screenSnapX = 130;
+    private int screenSnapY = 65;
 
 
-    void Start()
-    {
+    void Start() {
         psm = GameObject.Find("Player").GetComponent<PlayerStateManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.SetPositionAndRotation(new Vector3(Mathf.Round(psm.trans.position.x/screenSnapX)*screenSnapX,Mathf.Round(psm.trans.position.y/screenSnapY)*screenSnapY, -10), new Quaternion(0,0,0,1));
+        transform.SetPositionAndRotation(new Vector3(Mathf.Round(psm.trans.position.x/screenSnapX)*screenSnapX,Mathf.Round(psm.trans.position.y/screenSnapY)*screenSnapY, transform.position.z), new Quaternion(0,0,0,1));
     }
 }
